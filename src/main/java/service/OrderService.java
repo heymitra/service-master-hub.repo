c5ddc26fc291848915f631d6dedff084d4047b48
mitrahe <heydarimitra98@gmail.com>
2023-10-13 05:26:03 +0200

    update order service.

--- src/main/java/service/OrderService.java
+++ src/main/java/service/OrderService.java
@@ -2,6 +2,11 @@ package service;
 
 import base.service.BaseService;
 import entity.Order;
+import entity.User;
+import entity.enumeration.OrderStatusEnum;
+
+import java.time.LocalDateTime;
 
 public interface OrderService extends BaseService<Order, Long> {
+    Order save(String email, Long subServiceId, double proposedPrice, String workDescription, String address, LocalDateTime completionDateTime, OrderStatusEnum status);
 }
--- src/main/java/service/impl/OrderServiceImpl.java
+++ src/main/java/service/impl/OrderServiceImpl.java
@@ -1,13 +1,36 @@
 package service.impl;
 
 import base.service.impl.BaseServiceImpl;
+import entity.Customer;
 import entity.Order;
+import entity.SubService;
+import entity.User;
+import entity.enumeration.OrderStatusEnum;
 import repository.OrderRepository;
 import service.OrderService;
+import util.ApplicationContext;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class OrderServiceImpl extends BaseServiceImpl<Order, Long, OrderRepository>
         implements OrderService {
     public OrderServiceImpl(OrderRepository repository) {
         super(repository);
+    }
+
+    @Override
+    public Order save(String email, Long subServiceId, double proposedPrice, String workDescription, String address, LocalDateTime completionDateTime, OrderStatusEnum status) {
+        Order order = new Order();
+        Optional<SubService> subService = ApplicationContext.getSubServiceService().findById(subServiceId);
+        order.setSubService(subService.get());
+        User userByEmail = ApplicationContext.getUserService().findUserByEmail(email);
+        order.setCustomer((Customer) userByEmail);
+        order.setProposedPrice(proposedPrice);
+        order.setWorkDescription(workDescription);
+        order.setCompletionDateTime(completionDateTime);
+        order.setAddress(address);
+        order.setStatus(status);
+        return super.save(order);
     }
 }
