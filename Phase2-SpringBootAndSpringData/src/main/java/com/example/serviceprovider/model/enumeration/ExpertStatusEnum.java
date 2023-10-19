7c796865205464ec8588b283842fd904f11fe7c7
mitrahe <heydarimitra98@gmail.com>
2023-10-20 00:13:36 +0200

    Add enums.

---
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/model/enumeration/ExpertStatusEnum.java
@@ -0,0 +1,17 @@
+package com.example.serviceprovider.model.enumeration;
+
+public enum ExpertStatusEnum {
+    NEW("New"),
+    AWAITING_APPROVAL("Awaiting Approval"),
+    APPROVED("Approved");
+
+    private final String displayName;
+
+    ExpertStatusEnum(String displayName) {
+        this.displayName = displayName;
+    }
+
+//    public String getDisplayName() {
+//        return displayName;
+//    }
+}
---
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/model/enumeration/OrderStatusEnum.java
@@ -0,0 +1,20 @@
+package com.example.serviceprovider.model.enumeration;
+
+public enum OrderStatusEnum {
+    WAITING_FOR_OFFERS("Waiting for Offers"),
+    WAITING_FOR_EXPERT_SELECTION("Waiting for Expert Selection"),
+    WAITING_FOR_EXPERT_TO_COME("Waiting for the Expert to Come"),
+    STARTED("Started"),
+    COMPLETED("Completed"),
+    PAID("Paid");
+
+    private final String displayName;
+
+    OrderStatusEnum(String displayName) {
+        this.displayName = displayName;
+    }
+
+//    public String getDisplayName() {
+//        return displayName;
+//    }
+}
