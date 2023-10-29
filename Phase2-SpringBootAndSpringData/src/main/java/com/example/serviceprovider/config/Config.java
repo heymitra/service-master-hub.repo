ce19a0bc1df7cd8007a0ce53b1296e0f7886964a
mitrahe <heydarimitra98@gmail.com>
2023-10-29 06:10:22 +0100

    Add custom mapping.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/config/Config.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/config/Config.java
@@ -1,5 +1,9 @@
 package com.example.serviceprovider.config;
 
+import com.example.serviceprovider.base.BaseEntity;
+import com.example.serviceprovider.dto.UserResponseDto;
+import com.example.serviceprovider.model.Expert;
+import com.example.serviceprovider.model.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +13,16 @@ public class Config {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.createTypeMap(Expert.class, UserResponseDto.class)
+                .addMapping(BaseEntity::getId, UserResponseDto::setId)
+                .addMapping(User::getName, UserResponseDto::setName)
+                .addMapping(User::getSurname, UserResponseDto::setSurname)
+                .addMapping(User::getEmail, UserResponseDto::setEmail)
+                .addMapping(User::getRegistrationDateTime, UserResponseDto::setRegistrationDateTime);
+
+
+        return modelMapper;
     }
 }
