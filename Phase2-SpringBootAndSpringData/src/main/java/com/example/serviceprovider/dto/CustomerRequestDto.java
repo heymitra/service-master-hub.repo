20497ec9552ba180a2b7027f93d9b0072ec1b3c5
mitrahe <heydarimitra98@gmail.com>
2023-10-31 06:39:59 +0100

    Creat request and response DTO for customer.

---
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/dto/CustomerRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.serviceprovider.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CustomerRequestDto extends UserRequestDto {
+}
