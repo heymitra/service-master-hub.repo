739317e366f8166aded583681120146581be51b1
mitrahe <heydarimitra98@gmail.com>
2023-11-01 23:33:08 +0100

    Register user.

---
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/controller/ExpertController.java
@@ -0,0 +1,54 @@
+package com.example.serviceprovider.controller;
+
+import com.example.serviceprovider.dto.ExpertRequestDto;
+import com.example.serviceprovider.dto.ExpertResponseDto;
+import com.example.serviceprovider.exception.DuplicatedInstanceException;
+import com.example.serviceprovider.exception.EmptyFileException;
+import com.example.serviceprovider.model.Expert;
+import com.example.serviceprovider.service.ExpertService;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/expert")
+public class ExpertController {
+    private final ExpertService expertService;
+    private final ModelMapper modelMapper;
+
+    public ExpertController(ExpertService expertService, ModelMapper modelMapper) {
+        this.expertService = expertService;
+        this.modelMapper = modelMapper;
+    }
+
+    // http://localhost:8080/expert/register
+    @PostMapping("/register")
+    public ResponseEntity<ExpertResponseDto> register(@ModelAttribute @Valid ExpertRequestDto expertRequestDto) {
+        MultipartFile personalPhotoFile = expertRequestDto.getPersonalPhoto();
+        byte[] personalPhotoData = null;
+
+        if (personalPhotoFile != null && !personalPhotoFile.isEmpty()) {
+            try {
+                personalPhotoData = personalPhotoFile.getBytes();
+            } catch (IOException e) {
+                throw new EmptyFileException("empty image file");
+            }
+        }
+        Expert expert = modelMapper.map(expertRequestDto, Expert.class);
+        expert.setPersonalPhoto(personalPhotoData);
+        Expert addedExpert;
+        try {
+            addedExpert = expertService.save(expert);
+        } catch (DataIntegrityViolationException e) {
+            throw new DuplicatedInstanceException(String.format("%s is duplicated.", expert.getEmail()));
+        }
+        ExpertResponseDto expertResponseDto = modelMapper.map(addedExpert, ExpertResponseDto.class);
+        return new ResponseEntity<>(expertResponseDto, HttpStatus.CREATED);
+    }
+}
---
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/dto/UserResponseDto.java
@@ -0,0 +1,20 @@
+package com.example.serviceprovider.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDto {
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private LocalDateTime registrationDateTime;
+}
