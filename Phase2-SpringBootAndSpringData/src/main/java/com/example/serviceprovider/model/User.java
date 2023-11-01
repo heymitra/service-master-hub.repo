ef714400389ce7ce626dbd859d713416fc4c06c2
mitrahe <heydarimitra98@gmail.com>
2023-11-01 23:34:04 +0100

    Add credit field.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/model/User.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/model/User.java
@@ -2,8 +2,6 @@ package com.example.serviceprovider.model;
 
 import com.example.serviceprovider.base.BaseEntity;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,18 +14,15 @@ import java.time.LocalDateTime;
 @Table(name = "users")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "user_type", discriminatorType = DiscriminatorType.STRING)
-public abstract class User extends BaseEntity<Long>  {
-    @NotNull(message = "Name cannot be null")
+public abstract class User extends BaseEntity<Long> {
     private String name;
-
-    @NotNull(message = "Surname cannot be null")
     private String surname;
-
-    @NotNull(message = "Password cannot be null")
     private String password;
 
     @Column(unique = true)
-    @Email
     private String email;
+
     private LocalDateTime registrationDateTime;
+
+    private double credit;
 }
