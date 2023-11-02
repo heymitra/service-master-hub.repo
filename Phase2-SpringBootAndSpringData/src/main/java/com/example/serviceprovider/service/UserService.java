6864fa4c997394706222af671a7d7e507b2d5b41
mitrahe <heydarimitra98@gmail.com>
2023-11-02 10:02:12 +0100

    Remove unnecessary methods (change password and email uniqueness checker).

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/service/UserService.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/service/UserService.java
@@ -1,15 +1,13 @@
 package com.example.serviceprovider.service;
 
-import com.example.serviceprovider.exception.InvalidFormatException;
 import com.example.serviceprovider.model.User;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
+    User update(User user);
     Optional<User> findById(Long id);
     List<User> findAll();
     void deleteById(User user);
-    void changePassword(User user, String newPass) throws InvalidFormatException;
-    boolean isEmailUnique(String email);
 }
