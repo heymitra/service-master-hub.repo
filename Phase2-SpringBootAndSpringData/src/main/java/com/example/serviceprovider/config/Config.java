d0b1495b34f39aaf5162f641a363578d1d888666
mitrahe <heydarimitra98@gmail.com>
2023-11-02 09:52:03 +0100

    Remove custom mapper.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/config/Config.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/config/Config.java
@@ -1,9 +1,5 @@
 package com.example.serviceprovider.config;
 
-import com.example.serviceprovider.base.BaseEntity;
-import com.example.serviceprovider.dto.UserResponseDto;
-import com.example.serviceprovider.model.Expert;
-import com.example.serviceprovider.model.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,16 +9,7 @@ public class Config {
 
     @Bean
     public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
 
-        modelMapper.createTypeMap(Expert.class, UserResponseDto.class)
-                .addMapping(BaseEntity::getId, UserResponseDto::setId)
-                .addMapping(User::getName, UserResponseDto::setName)
-                .addMapping(User::getSurname, UserResponseDto::setSurname)
-                .addMapping(User::getEmail, UserResponseDto::setEmail)
-                .addMapping(User::getRegistrationDateTime, UserResponseDto::setRegistrationDateTime);
-
-
-        return modelMapper;
+        return new ModelMapper();
     }
 }
