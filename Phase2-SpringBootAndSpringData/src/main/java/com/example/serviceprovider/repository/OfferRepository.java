4319dcfdffc2725259d505aae1f82f8a80fa1c5e
mitrahe <heydarimitra98@gmail.com>
2023-11-02 09:54:12 +0100

    Refactor input type.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/repository/OfferRepository.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/repository/OfferRepository.java
@@ -1,7 +1,6 @@
 package com.example.serviceprovider.repository;
 
 import com.example.serviceprovider.model.Offer;
-import com.example.serviceprovider.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface OfferRepository extends JpaRepository<Offer, Long> {
-    @Query("SELECT o FROM Offer o WHERE o.order = :order")
-    List<Offer> findOffersByOrder(@Param("order") Order order);
+    @Query("SELECT o FROM Offer o WHERE o.order.id = :orderId")
+    List<Offer> findOffersByOrder(@Param("orderId") Long orderId);
 }
