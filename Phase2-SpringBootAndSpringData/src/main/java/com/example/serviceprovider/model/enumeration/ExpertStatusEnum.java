41660bef6401426f9ebba5fb653718a2c2944d04
mitrahe <heydarimitra98@gmail.com>
2023-11-02 09:51:20 +0100

    Add new status to expert status enum and remove constructor.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/model/enumeration/ExpertStatusEnum.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/model/enumeration/ExpertStatusEnum.java
@@ -1,17 +1,8 @@
 package com.example.serviceprovider.model.enumeration;
 
 public enum ExpertStatusEnum {
-    NEW("New"),
-    AWAITING_APPROVAL("Awaiting Approval"),
-    APPROVED("Approved");
-
-    private final String displayName;
-
-    ExpertStatusEnum(String displayName) {
-        this.displayName = displayName;
-    }
-
-//    public String getDisplayName() {
-//        return displayName;
-//    }
+    NEW,
+    AWAITING_APPROVAL,
+    APPROVED,
+    INACTIVE;
 }
