3b651c20564a6e32ba8bc974692b1601990f3fa2
mitrahe <heydarimitra98@gmail.com>
2023-11-03 06:58:46 +0100

    Add captcha to online payment.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/controller/OrderController.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.example.serviceprovider.controller;
 
+import cn.apiclub.captcha.Captcha;
+import cn.apiclub.captcha.gimpy.DropShadowGimpyRenderer;
 import com.example.serviceprovider.dto.OrderRequestDto;
 import com.example.serviceprovider.dto.OrderByIdsDto;
 import com.example.serviceprovider.dto.OrderResponseDto;
@@ -9,12 +11,17 @@ import com.example.serviceprovider.model.Offer;
 import com.example.serviceprovider.model.Order;
 import com.example.serviceprovider.model.enumeration.OrderStatusEnum;
 import com.example.serviceprovider.service.OrderService;
+import com.example.serviceprovider.utility.ImageUtility;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.image.BufferedImage;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,10 +32,12 @@ import java.util.List;
 public class OrderController {
     private final OrderService orderService;
     private final ModelMapper modelMapper;
+    private final ImageUtility imageUtility;
 
-    public OrderController(OrderService orderService, ModelMapper modelMapper) {
+    public OrderController(OrderService orderService, ModelMapper modelMapper, ImageUtility imageUtility) {
         this.orderService = orderService;
         this.modelMapper = modelMapper;
+        this.imageUtility = imageUtility;
     }
 
     @GetMapping("/payment-form")
@@ -93,24 +102,24 @@ public class OrderController {
         return new ResponseEntity<>(orderResponseDto, HttpStatus.OK);
     }
 
-    @PostMapping("/online-payment")
-    public ResponseEntity<String> makePayment(
-            @RequestParam String cardNumber,
-            @RequestParam String cvv2,
-            @RequestParam String secondPassword,
-            @RequestParam Long orderId
-    ) {
-        if (!isValidCardInfo(cardNumber, cvv2, secondPassword)) {
-            return new ResponseEntity<>("Invalid card information. Payment failed.", HttpStatus.BAD_REQUEST);
-        }
-
-        Order toBePayedOrder = getToBePayedOrder(orderId);
-
-        orderService.makePayment(toBePayedOrder);
-
-        System.out.println(cardNumber + cvv2 + secondPassword);
-        return ResponseEntity.ok("Payment successful");
-    }
+//    @PostMapping("/online-payment")
+//    public ResponseEntity<String> makePayment(
+//            @RequestParam String cardNumber,
+//            @RequestParam String cvv2,
+//            @RequestParam String secondPassword,
+//            @RequestParam Long orderId
+//    ) {
+//        if (!isValidCardInfo(cardNumber, cvv2, secondPassword)) {
+//            return new ResponseEntity<>("Invalid card information. Payment failed.", HttpStatus.BAD_REQUEST);
+//        }
+//
+//        Order toBePayedOrder = getToBePayedOrder(orderId);
+//
+//        orderService.makePayment(toBePayedOrder);
+//
+//        System.out.println(cardNumber + cvv2 + secondPassword);
+//        return ResponseEntity.ok("Payment successful");
+//    }
 
     @GetMapping("/credit-payment")
     public ResponseEntity<OrderResponseDto> payWithCredit(@RequestParam Long orderId) {
@@ -145,6 +154,56 @@ public class OrderController {
         return cardNumber != null && cardNumber.matches("\\d{16}")
                 && cvv2 != null && cvv2.matches("\\d{3,4}")
                 && secondPassword != null && secondPassword.matches("\\d{8,12}");
+    }
+
+    @PostMapping("/online-payment")
+    public ResponseEntity<String> makePayment(
+            @RequestParam String cardNumber,
+            @RequestParam String cvv2,
+            @RequestParam String password,
+            @RequestParam Long orderId,
+            @RequestParam String captcha,
+            HttpSession session
+    ) {
+        System.out.println("Hi");
+        String storedCaptcha = (String) session.getAttribute("captcha");
+
+        if (!captcha.equals(storedCaptcha)) {
+            return new ResponseEntity<>("Wrong captcha. ", HttpStatus.BAD_REQUEST);
+        }
+
+        if (!isValidCardInfo(cardNumber, cvv2, password)) {
+            return new ResponseEntity<>("Invalid card information. Payment failed.", HttpStatus.BAD_REQUEST);
+        }
+
+        Order toBePayedOrder = getToBePayedOrder(orderId);
+
+        orderService.makePayment(toBePayedOrder);
+
+        System.out.println(cardNumber + cvv2 + password);
+        return ResponseEntity.ok("Payment successful");
+    }
+
+    @GetMapping("/captcha-image")
+    public ResponseEntity<byte[]> generateCaptchaImage(HttpSession session) {
+        Captcha captcha = new Captcha.Builder(200, 50)
+                .addText()
+                .addNoise()
+                .addBackground()
+                .addBorder()
+                .gimp(new DropShadowGimpyRenderer())
+                .build();
+
+        session.setAttribute("captcha", captcha.getAnswer());
+
+        BufferedImage image = captcha.getImage();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+
+        byte[] imageBytes = imageUtility.convertImageToByteArray(image);
+
+        return new ResponseEntity<>(imageBytes, headers, 200);
     }
 }
 
