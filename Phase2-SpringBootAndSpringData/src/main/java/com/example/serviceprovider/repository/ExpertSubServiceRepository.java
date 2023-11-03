c826f3037c5d5e5a7a5f8a1f8fe619f58657eb7b
mitrahe <heydarimitra98@gmail.com>
2023-11-03 02:55:05 +0100

    Add repository for joint entity.

---
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/repository/ExpertSubServiceRepository.java
@@ -0,0 +1,7 @@
+package com.example.serviceprovider.repository;
+
+import com.example.serviceprovider.model.ExpertSubService;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpertSubServiceRepository extends JpaRepository<ExpertSubService, Long> {
+}
