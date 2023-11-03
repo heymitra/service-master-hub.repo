82304df3b6c5e0825e38865d6701467a87719634
mitrahe <heydarimitra98@gmail.com>
2023-11-03 02:58:04 +0100

    Create controller for review and implement add method.

---
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/controller/ReviewController.java
@@ -0,0 +1,32 @@
+package com.example.serviceprovider.controller;
+
+import com.example.serviceprovider.dto.ReviewRequestDto;
+import com.example.serviceprovider.dto.ReviewResponseDto;
+import com.example.serviceprovider.model.Review;
+import com.example.serviceprovider.service.ReviewService;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/review")
+public class ReviewController {
+    private final ReviewService service;
+    private final ModelMapper modelMapper;
+
+    public ReviewController(ReviewService service, ModelMapper modelMapper) {
+        this.service = service;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ReviewResponseDto> add(@RequestBody @Valid ReviewRequestDto reviewRequestDto,
+                                                 @RequestParam Long orderId) {
+        Review review = modelMapper.map(reviewRequestDto, Review.class);
+        Review addedReview = service.add(review, orderId);
+        ReviewResponseDto reviewResponseDto = modelMapper.map(addedReview, ReviewResponseDto.class);
+        return new ResponseEntity<>(reviewResponseDto, HttpStatus.CREATED);
+    }
+}
