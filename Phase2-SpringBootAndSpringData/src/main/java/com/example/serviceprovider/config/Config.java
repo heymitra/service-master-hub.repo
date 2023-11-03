bea5f12c2f67d3c25b6aa13434fe7070d6efb344
mitrahe <heydarimitra98@gmail.com>
2023-11-03 02:41:47 +0100

    Add explicit mapping for order.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/config/Config.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/config/Config.java
@@ -1,5 +1,7 @@
 package com.example.serviceprovider.config;
 
+import com.example.serviceprovider.dto.OrderResponseDto;
+import com.example.serviceprovider.model.Order;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,15 @@ public class Config {
     @Bean
     public ModelMapper modelMapper() {
 
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+
+        modelMapper.createTypeMap(Order.class, OrderResponseDto.class)
+                .addMapping(src -> src.getProposedPrice(), OrderResponseDto::setProposedPrice)
+                .addMapping(src -> src.getWorkDescription(), OrderResponseDto::setWorkDescription)
+                .addMapping(src -> src.getCompletionDateTime(), OrderResponseDto::setCompletionDateTime)
+                .addMapping(src -> src.getAddress(), OrderResponseDto::setAddress)
+                .addMapping(src -> src.getStatus(), OrderResponseDto::setStatus);
+
+        return modelMapper;
     }
 }
