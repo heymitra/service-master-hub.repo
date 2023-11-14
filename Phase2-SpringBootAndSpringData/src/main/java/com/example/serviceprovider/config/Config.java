6a885aafaaae51d9e9da86f150e00ddd7a3769e5
mitrahe <heydarimitra98@gmail.com>
2023-11-15 00:48:17 +0100

    Config Java Mail Sender.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/config/Config.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/config/Config.java
@@ -1,25 +1,19 @@
 package com.example.serviceprovider.config;
 
-import com.example.serviceprovider.dto.OrderResponseDto;
-import com.example.serviceprovider.model.Order;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Properties;
 
 @Configuration
 public class Config {
 
     @Bean
     public ModelMapper modelMapper() {
-
-//                modelMapper.createTypeMap(Order.class, OrderResponseDto.class)
-//                .addMapping(Order::getProposedPrice, OrderResponseDto::setProposedPrice)
-//                .addMapping(Order::getWorkDescription, OrderResponseDto::setWorkDescription)
-//                .addMapping(Order::getCompletionDateTime, OrderResponseDto::setCompletionDateTime)
-//                .addMapping(Order::getAddress, OrderResponseDto::setAddress)
-//                .addMapping(Order::getStatus, OrderResponseDto::setStatus);
-
         return new ModelMapper();
     }
 
@@ -27,4 +21,22 @@ public class Config {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("my-email@gmail.com");
+        mailSender.setPassword("my-app-password");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
 }
