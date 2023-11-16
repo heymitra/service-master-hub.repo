9b028448f339ac12c400c9f86163a0995f0d2f5c
mitrahe <heydarimitra98@gmail.com>
2023-11-16 09:43:14 +0100

    Implement activate method to activate email address.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/service/impl/UserServiceImpl.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/service/impl/UserServiceImpl.java
@@ -1,28 +1,38 @@
 package com.example.serviceprovider.service.impl;
 
+import com.example.serviceprovider.exception.InvalidInputException;
+import com.example.serviceprovider.model.Customer;
+import com.example.serviceprovider.model.UserToken;
+import com.example.serviceprovider.model.Expert;
 import com.example.serviceprovider.model.User;
+import com.example.serviceprovider.model.enumeration.Role;
 import com.example.serviceprovider.repository.UserRepository;
+import com.example.serviceprovider.service.CustomerService;
+import com.example.serviceprovider.service.UserTokenService;
+import com.example.serviceprovider.service.ExpertService;
 import com.example.serviceprovider.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@RequiredArgsConstructor
+@Transactional
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository repository;
     private final BCryptPasswordEncoder passwordEncoder;
-
-    public UserServiceImpl(UserRepository repository, BCryptPasswordEncoder passwordEncoder) {
-        this.repository = repository;
-        this.passwordEncoder = passwordEncoder;
-    }
+    private final UserTokenService userTokenService;
+    private final ExpertService expertService;
+    private final CustomerService customerService;
 
     @Override
-    public User update(User user){
+    public User update(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return repository.save(user);
     }
@@ -42,13 +52,33 @@ public class UserServiceImpl implements UserService {
         repository.deleteById(user.getId());
     }
 
-
-    public Page<User> searchAndFilterUsers(String role,
+    @Override
+    public Page<User> searchAndFilterUsers(Role role,
                                            String name,
                                            String surname,
                                            String email,
                                            String sortBy,
                                            Pageable pageable) {
         return repository.searchAndFilterUsers(role, name, surname, email, sortBy, pageable);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return repository.findByEmail(email);
+    }
+
+    @Override
+    public void activate(String token) {
+        UserToken emailActivator = userTokenService.findByActivationToken(token)
+                .orElseThrow(() -> new InvalidInputException("Invalid or expired verification code."));
+
+        User user = emailActivator.getUser();
+        userTokenService.delete(emailActivator);
+
+        if (user.getRole().equals(Role.ROLE_EXPERT)) {
+            expertService.activate((Expert) user);
+        } else if (user.getRole().equals(Role.ROLE_CUSTOMER)) {
+            customerService.activate((Customer) user);
+        }
     }
 }
