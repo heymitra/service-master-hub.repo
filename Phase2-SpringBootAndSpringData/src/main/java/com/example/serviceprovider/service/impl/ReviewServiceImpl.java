f6112aa5c4187bbc5c49b8f493b4f922b670301e
mitrahe <heydarimitra98@gmail.com>
2023-11-16 12:41:40 +0100

    Enhance 'add' method in ReviewService with customer validation.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/service/impl/ReviewServiceImpl.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/service/impl/ReviewServiceImpl.java
@@ -1,34 +1,34 @@
 package com.example.serviceprovider.service.impl;
 
 import com.example.serviceprovider.exception.InvalidInputException;
-import com.example.serviceprovider.model.Expert;
-import com.example.serviceprovider.model.Offer;
-import com.example.serviceprovider.model.Order;
-import com.example.serviceprovider.model.Review;
+import com.example.serviceprovider.model.*;
 import com.example.serviceprovider.model.enumeration.OrderStatusEnum;
 import com.example.serviceprovider.repository.ReviewRepository;
 import com.example.serviceprovider.service.ExpertService;
 import com.example.serviceprovider.service.OrderService;
 import com.example.serviceprovider.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
+@Transactional
 @Service
 public class ReviewServiceImpl implements ReviewService {
     private final ReviewRepository repository;
     private final OrderService orderService;
     private final ExpertService expertService;
 
-    public ReviewServiceImpl(ReviewRepository repository, OrderService orderService, ExpertService expertService) {
-        this.repository = repository;
-        this.orderService = orderService;
-        this.expertService = expertService;
-    }
-
     @Override
     public Review add(Review review, Long orderId) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
 
         Order order = orderService.findById(orderId)
                 .orElseThrow(() -> new InvalidInputException("Order with ID " + orderId + " not found."));
+
+        if(!email.equals(order.getCustomer().getEmail()))
+            throw new InvalidInputException("You are not allowed to add review on this order.");
 
         OrderStatusEnum status = order.getStatus();
         if (status != OrderStatusEnum.PAID && status != OrderStatusEnum.COMPLETED)
