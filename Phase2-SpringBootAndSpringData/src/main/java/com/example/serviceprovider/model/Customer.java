22edcdac0adb3651a31bc1e4e5c6fb6cd42e8f9e
mitrahe <heydarimitra98@gmail.com>
2023-11-19 20:14:09 +0100

    Add Order relationship to Customer entity.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/model/Customer.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/model/Customer.java
@@ -1,10 +1,22 @@
 package com.example.serviceprovider.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
 
 @Entity
 @DiscriminatorValue("Customer")
 public class Customer extends User {
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Order> orders = new ArrayList<>();
 }
 
