4bc94e1efa1b72dec160d3def047c1f8a7136354
mitrahe <heydarimitra98@gmail.com>
2023-11-21 00:25:47 +0100

    Remove unnecessary findById method.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/controller/ServiceController.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/controller/ServiceController.java
@@ -3,7 +3,6 @@ package com.example.serviceprovider.controller;
 import com.example.serviceprovider.dto.ServiceRequestDto;
 import com.example.serviceprovider.dto.ServiceResponseDto;
 import com.example.serviceprovider.exception.DuplicatedInstanceException;
-import com.example.serviceprovider.exception.ItemNotFoundException;
 import com.example.serviceprovider.model.Service;
 import com.example.serviceprovider.service.ServiceService;
 import jakarta.validation.Valid;
@@ -12,8 +11,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/service")
@@ -26,7 +23,6 @@ public class ServiceController {
         this.modelMapper = modelMapper;
     }
 
-    // http://localhost:8080/service/add
     @PostMapping("/add")
     public ResponseEntity<ServiceResponseDto> add(@RequestBody @Valid ServiceRequestDto serviceRequestDto) {
         Service service = modelMapper.map(serviceRequestDto, Service.class);
@@ -38,17 +34,5 @@ public class ServiceController {
         }
         ServiceResponseDto serviceResponseDto = modelMapper.map(addedService, ServiceResponseDto.class);
         return new ResponseEntity<>(serviceResponseDto, HttpStatus.CREATED);
-    }
-
-    @GetMapping("/find-by-id")
-    public ResponseEntity<ServiceResponseDto> findById(@RequestParam Long id) {
-        Optional<Service> optionalService = serviceService.findById(id);
-        if (optionalService.isPresent()) {
-            Service service = optionalService.get();
-            ServiceResponseDto serviceResponseDto = modelMapper.map(service, ServiceResponseDto.class);
-
-            return new ResponseEntity<>(serviceResponseDto, HttpStatus.OK);
-        }
-        throw new ItemNotFoundException(String.format("%s is not a valid service ID", id));
     }
 }
