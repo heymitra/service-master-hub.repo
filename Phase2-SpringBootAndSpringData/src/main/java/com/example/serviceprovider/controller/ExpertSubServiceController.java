387535837703c8bbb3e96801944a6853b88060b1
mitrahe <heydarimitra98@gmail.com>
2023-11-21 01:28:17 +0100

    Update ExpertSubServiceController.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/controller/ExpertSubServiceController.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/controller/ExpertSubServiceController.java
@@ -1,6 +1,6 @@
 package com.example.serviceprovider.controller;
 
-import com.example.serviceprovider.dto.ExpertSubServiceResponseDTO;
+import com.example.serviceprovider.dto.ExpertSubServiceResponseDto;
 import com.example.serviceprovider.exception.ItemNotFoundException;
 import com.example.serviceprovider.model.ExpertSubService;
 import com.example.serviceprovider.service.ExpertSubServiceService;
@@ -18,29 +18,19 @@ public class ExpertSubServiceController {
     }
 
     @PostMapping("/assign")
-    public ResponseEntity<ExpertSubServiceResponseDTO> assign(@RequestParam Long expertId,
-                                                @RequestParam Long subServiceId) {
+    public ResponseEntity<ExpertSubServiceResponseDto> assign(@RequestParam Long expertId,
+                                                              @RequestParam Long subServiceId) {
         ExpertSubService savedRecord = service.save(expertId, subServiceId);
         if (savedRecord == null) {
             throw new ItemNotFoundException(String.format("Expert-ID = %s / Sub-Service_ID = %s not found", expertId, subServiceId));
         }
-        ExpertSubServiceMapper mapper = new ExpertSubServiceMapper();
-        ExpertSubServiceResponseDTO savedRecordDTO = mapper.modelToDto(savedRecord);
+        ExpertSubServiceResponseDto mapper = new ExpertSubServiceResponseDto();
+        ExpertSubServiceResponseDto savedRecordDTO = mapper.modelToDto(savedRecord);
         return new ResponseEntity<>(savedRecordDTO, HttpStatus.CREATED);
-
     }
 
     @DeleteMapping("remove")
     public void delete (@RequestParam Long expertId, @RequestParam Long subServiceId) {
         service.remove(expertId, subServiceId);
-    }
-}
-
-class ExpertSubServiceMapper {
-    public ExpertSubServiceResponseDTO modelToDto (ExpertSubService expertSubService) {
-        return new ExpertSubServiceResponseDTO (expertSubService.getExpert().getId(),
-                expertSubService.getExpert().getName(),
-                expertSubService.getSubService().getId(),
-                expertSubService.getSubService().getSubServiceName());
     }
 }
