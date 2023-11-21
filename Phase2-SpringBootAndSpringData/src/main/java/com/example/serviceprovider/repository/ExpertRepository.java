255e50bda7b1937b4b9720dcfbbf178ad8b12fab
mitrahe <heydarimitra98@gmail.com>
2023-11-21 01:00:28 +0100

    Remove unnecessary findAllExperts method.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/repository/ExpertRepository.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/repository/ExpertRepository.java
@@ -2,16 +2,11 @@ package com.example.serviceprovider.repository;
 
 import com.example.serviceprovider.model.Expert;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ExpertRepository extends JpaRepository<Expert, Long> {
-    @Query("SELECT e FROM Expert e")
-    List<Expert> findAllExperts();
-
     Optional<Expert> findByEmail(String email);
 }
