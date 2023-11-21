a2fe8bae31169282ca6f033660d7176389094674
mitrahe <heydarimitra98@gmail.com>
2023-11-21 09:40:07 +0100

    Add more fields to OrderResponseDto.

--- Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/dto/OrderResponseDTO.java
+++ Phase2-SpringBootAndSpringData/src/main/java/com/example/serviceprovider/dto/OrderResponseDTO.java
@@ -1,10 +1,12 @@
 package com.example.serviceprovider.dto;
 
+import com.example.serviceprovider.model.Order;
 import com.example.serviceprovider.model.enumeration.OrderStatusEnum;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.modelmapper.ModelMapper;
 
 import java.time.LocalDateTime;
 
@@ -13,10 +15,17 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderResponseDto {
-    private Long id;
+    private Long orderId;
+    private SubServiceResponseDto subService;
     private double proposedPrice;
     private String workDescription;
-    private LocalDateTime completionDateTime;
+    private LocalDateTime expectedCompletionTime;
+    private LocalDateTime applyTime;
+    private LocalDateTime realCompletionTime;
     private String address;
     private OrderStatusEnum status;
+
+    public OrderResponseDto fromOrder(Order order, ModelMapper modelMapper) {
+        return modelMapper.map(order, OrderResponseDto.class);
+    }
 }
